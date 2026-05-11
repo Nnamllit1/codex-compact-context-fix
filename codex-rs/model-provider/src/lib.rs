srpatcha/codex

feat: use thread agent task auth for inference

codex-rs/model-provider/src/lib.rs
@@ -3,7 +3,10 @@ mod auth;
 mod bearer_auth_provider;
 mod provider;
 
+pub use auth::AgentTaskAuth;
+pub use auth::auth_provider_from_agent_task;
 pub use auth::auth_provider_from_auth;
+pub use auth::provider_uses_first_party_auth_path;
 pub use auth::unauthenticated_auth_provider;
 pub use bearer_auth_provider::BearerAuthProvider;
 pub use bearer_auth_provider::BearerAuthProvider as CoreAuthProvider;

Nnamllit1/codex-compact-context-fix

codex-rs/model-provider/src/lib.rs
@@ -4,7 +4,12 @@ mod bearer_auth_provider;
 mod models_endpoint;
 mod provider;
 
+pub use codex_agent_identity::AgentTaskExternalRef;
+
+pub use auth::ProviderAuthScope;
+pub use auth::auth_provider_from_agent_task;
 pub use auth::auth_provider_from_auth;
+pub use auth::provider_uses_first_party_auth_path;
 pub use auth::unauthenticated_auth_provider;
 pub use bearer_auth_provider::BearerAuthProvider;
 pub use bearer_auth_provider::BearerAuthProvider as CoreAuthProvider;